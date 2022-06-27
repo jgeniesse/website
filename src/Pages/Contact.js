c549d1d5f3ea5a5367d279be0a94056f787320f2
Jon Geniesse <jageniesse@wisc.edu>
2022-06-27 15:05:24 -0500

    No more git pages stuff

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,12 +1,39 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from "react";
+import { Row, Col, Container, Form, Button } from "react-bootstrap";
+import "./Contact.css";
 
-export default function Contact(props){
-
+export default function Contact(props) {
   useEffect(() => {
     props.setHighlight("contact");
-  }, [])
+  }, []);
 
   return (
-    <div>Contact</div>
-  )
+    <Row className="g-0">
+      <Col md={6} lg={6} xl={7}>
+      <h1 className="contactMe">Contact Me!</h1>
+        <div className="div4">
+          <Form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Full Name</Form.Label>
+              <Form.Control type="text" placeholder="John Smith" required/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" required/>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={4} placeholder="Your Message" required/>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Send Message
+            </Button>
+          </Form>
+          </div>
+      </Col>
+      <Col md={6} lg={6} xl={5} style={{ textAlign: "right"}}>
+        <img src="./images/map.png" className="map"></img>
+      </Col>
+    </Row>
+  );
 }
